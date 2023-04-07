cf011c1745f2f033280bf299ec26f53761204fae
kaveeshane20 <120721339+kaveeshane20@users.noreply.github.com>
2023-04-07 13:25:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var link = document.querySelector('.link');
+var format = document.querySelector('.format');
+
+function download() {
+    if(link.value!=""){
+        var url1 = link.value.replace("https://youtube.be/","https://www.youtube.com/embed/");
+
+        document.querySelector('.result1').innerHTML = `<iframe width="100%" height="190" src="${url1}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; picture-in-picture" allowfullscreen><iframe>`;
+
+        var url2 = `https://loader.to/api/button/?url=${link.value}&f=${format.value}`;
+
+        document.querySelector('.result2').innerHTML = `<iframe style="width:100%;height:60px;border:hidden;overflow:hidden;" scrolling="no" src="${url2}"><iframe`;
+    }
+}
